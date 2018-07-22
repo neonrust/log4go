d62db3ebceee588e7336e68dc03f6151b31bdd61
Tatsujin <github@0x1.se>
2018-07-23 01:13:41 +0200

    + added WatchedFileHandler; modeled after Python's counterpart. Re-opens the file when it is detected to have been moved/renamed (e.g. log rotation)   uses os.Stat() before each write; does of course incur a run-time penalty * StreamHandler (base class of all bundled handlers) now calls a onPreWrite() function before each write (default implementation does nothing)

--- handlers.go
+++ handlers.go
@@ -1,10 +1,12 @@
 package log4go
 
 import (
+	"fmt"
 	"io"
 	"os"
-	"fmt"
 	"runtime"
+	"syscall"
+	"sync/atomic"
 )
 
 // Handler handles the formatted log events.
@@ -17,25 +19,24 @@ type Handler interface {
 	Shutdown()
 }
 
-// StreamHandler handles file-based output
+// StreamHandler handles stream-based output.
 type StreamHandler struct {
 	writer        io.Writer
 	formatter     Formatter
 	level         Level
 	commitChannel chan Record
-	committerDone chan bool
+	committerStop chan struct{}
 	shutdown      bool
 }
 
 // NewStreamHandler returns a new StreamHandler instance using the specified writer.
-func NewStreamHandler(writer io.Writer) (*StreamHandler, error) {
+func NewStreamHandler(w io.Writer) (*StreamHandler, error) {
 	handler := &StreamHandler{
-		writer:        writer,
+		writer:        w,
 		commitChannel: make(chan Record, 100),
-		committerDone: make(chan bool),
+		committerStop: make(chan struct{}),
 		shutdown:      false,
 	}
-	//fmt.Printf("[%p] NewStreamHandler\n", handler)
 
 	go handler.committer()
 
@@ -43,7 +44,7 @@ func NewStreamHandler(writer io.Writer) (*StreamHandler, error) {
 }
 
 // NewFileHandler returns a new StreamHandler instance writing to the specified file name.
-func NewFileHandler(fileName string, append bool) (*StreamHandler, error) {
+func NewFileHandler(filename string, append bool) (*StreamHandler, error) {
 	flags := os.O_WRONLY | os.O_CREATE
 	if append {
 		flags |= os.O_APPEND
@@ -51,7 +52,7 @@ func NewFileHandler(fileName string, append bool) (*StreamHandler, error) {
 		flags |= os.O_TRUNC
 	}
 
-	writer, err := os.OpenFile(fileName, flags, 0664)
+	writer, err := os.OpenFile(filename, flags, 0664)
 	if err != nil {
 		return nil, err
 	}
@@ -70,43 +71,51 @@ func (h *StreamHandler) Level() Level {
 
 // Handle handles the formatted message.
 func (h *StreamHandler) Handle(rec *Record) error {
-	if ! h.shutdown {
+	if !h.shutdown { // TODO: should use mutex (to avoid writing to closed channel)
 		h.commitChannel <- *rec
-		//fmt.Printf("[%p] SteamHandler.Handle(): sent record: '%s'\n", h, rec.Message)
 	}
 	return nil
 }
 
+// Shutdown shuts down the handler.
 func (h *StreamHandler) Shutdown() {
-	if ! h.shutdown {
+	if !h.shutdown {
 		h.shutdown = true
-		close(h.commitChannel)
-		<-h.committerDone
-		runtime.GC()
+		h.committerStop <- struct{} // unbuffered; when this returns the committer has stopped
+
+		close(h.committerStop)
+		close(h.commitChannel) // TODO: see Handle() or never close this channel?
 	}
 }
 
+func (h *StreamHandler) onPreWrite() {
+	// default does nothing
+}
+
 func (h *StreamHandler) committer() {
-	//fmt.Printf("[%p] SteamHandler.committer(): running, ch = %p\n", h, h.commitChannel)
+	ff := h.Formatter()
 
-	for rec := range h.commitChannel {
-		//fmt.Printf("[%p] SteamHandler.committer(): got record, message: %s\n", h, rec.Message)
+	for {
+		select {
+		case rec := <-h.commitChannel:
+			msg, err := ff.Format(&rec)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "log4go.StreamHandler: formatter error %v\n", err)
+				continue
+			}
 
-		msg, err := h.Formatter().Format(&rec)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "log4go.SteamHandler: formatter error %v\n", err)
-			return
+			msg = append(msg, '\n')
+
+			h.onPreWrite()
+			
+			if _, err = h.writer.Write(msg); err != nil {
+				fmt.Fprintf(os.Stderr, "log4go.StreamHandler: write error: %v\n", err)
+			}
+
+		case <-h.committerStop:
+			break
 		}
-
-		msg = append(msg, '\n')
-		if _, err = h.writer.Write(msg); err != nil {
-			fmt.Fprintf(os.Stderr, "log4go.SteamHandler: write error: %v\n", err)
-		}
-
-		//fmt.Printf("[%p] SteamHandler.comitter(): wrote message: '%s'\n", h, rec.Message)
 	}
-	//fmt.Printf("[%p] SteamHandler.comitter(): exit\n", h)
-	h.committerDone <- true
 }
 
 // SetFormatter sets the handler's Formatter.
@@ -117,4 +126,88 @@ func (h *StreamHandler) SetFormatter(formatter Formatter) {
 // Formatter resutns the handler's Formatter.
 func (h *StreamHandler) Formatter() Formatter {
 	return h.formatter
+}
+
+// WatchedFileHandler watches the log file: if file is moved the filename is re-opened.
+type WatchedFileHandler struct {
+	*StreamHandler
+
+	fp       *os.File // we want to use Sync()
+	filename string
+	append   bool
+	inode    uint64
+	dev      uint64
+}
+
+// NewWatchedFileHandler returns a new WatchedFileHandler instance writing to the specified file name.
+func NewWatchedFileHandler(filename string, append bool) (*WatchedFileHandler, error) {
+	wfh := &WatchedFileHandler{
+		filename: filename,
+		append:   append,
+	}
+
+	if err := wfh.open(); err != nil {
+		return nil, err
+	}
+
+	s, err := NewStreamHandler(wfh.fp, append)
+	if err != nil {
+		return nil, err
+	}
+
+	wfh.StreamHandler = s
+
+	return wfh, nil
+}
+
+// called when committer is about to write a message
+func (h *WatchedFileHandler) onPreWrite() {
+	if h.fileHasMoved() {
+		// just re-open, with same filename
+		h.close()
+		h.open()
+		h.writer = h.fp
+	}
+}
+
+func (h WatchedFileHandler) fileHasMoved() bool {
+	// TODO: use fsnotify to detect when the file has moved?
+
+	dev, ino := h.statFile()
+	// in case statFile() returns (0, 0) this will return true also
+	return dev != h.dev || ino != h.inode
+}
+
+func (h *WatchedFileHandler) close() {
+	if h.fp != nil {
+		h.fp.Sync()
+		h.fp.Close()
+		h.fp = nil
+		h.writer = nil
+	}
+}
+
+func (h *WatchedFileHandler) open() error {
+	flags := os.O_WRONLY | os.O_CREATE
+	if h.append {
+		flags |= os.O_APPEND
+	} else {
+		flags |= os.O_TRUNC
+	}
+
+	fp, err = os.OpenFile(h.filename, flags, 0664)
+	if err != nil {
+		return err
+	}
+	h.writer = fp
+	
+	h.dev, h.inode = h.statFile()
+}
+
+func (h WatchedFileHandler) statFile() (uint64, uint64) {
+	info, _ := os.Stat(h.filename)
+	if stat, err := info.Sys().(*syscall.Stat_t); err != nil {
+		return 0, 0
+	}
+	return stat.Dev, stat.Ino
 }
